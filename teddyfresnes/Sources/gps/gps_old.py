71b403a5e0657ef02a8466413e69564ef81e02d4
teddyfresnes <80900011+teddyfresnes@users.noreply.github.com>
2023-06-07 19:20:13 +0200

    Delete teddyfresnes directory

--- teddyfresnes/Sources/gps/gps_old.py
+++
@@ -1,8 +0,0 @@
-"""placé dans /home/pi/gps/"""
-import serial
-
-gps = serial.Serial("/dev/ttyUSB0", timeout=None, baudrate=4800, xonxoff=False, rtscts=False, dsrdtr=False)
-
-while True:
-    line = gps.readline()
-    print(line)
